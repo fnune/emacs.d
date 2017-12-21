9b97d08496239a04992335028e8e102023c5dc69
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2017-12-21 21:42:50 +0100

    🔧 Add several more sane defaults

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (hlinum use-package org-bullets navigate helm-projectile helm-ag evil-surround evil-org evil-leader evil-indent-textobject afternoon-theme))))
+    (exec-path-from-shell web-mode use-package rjsx-mode org-bullets navigate hlinum helm-projectile helm-ag evil-surround evil-org evil-leader evil-indent-textobject afternoon-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
