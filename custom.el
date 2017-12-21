6059ba561d56d341c733b4cedf4f77195577c9e3
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2017-12-21 17:32:45 +0100

    ✨ Use tmux-navigator Emacs port for tmux panes

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (afternoon-theme zenburn-theme use-package solarized-theme org-bullets noctilux-theme monokai-theme moe-theme evil-surround evil-org evil-leader evil-indent-textobject dracula-theme doom-themes cyberpunk-theme color-theme-sanityinc-tomorrow ample-theme))))
+    (navigator zenburn-theme use-package solarized-theme org-bullets noctilux-theme navigate monokai-theme moe-theme evil-surround evil-org evil-leader evil-indent-textobject dracula-theme doom-themes cyberpunk-theme color-theme-sanityinc-tomorrow ample-theme afternoon-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
