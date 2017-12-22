b3e354b2237920a980654263d8925380699c2dce
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2017-12-22 14:19:41 +0100

    ✨ Add neotree and evil keybindings

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (drag-stuff web-mode use-package rjsx-mode org-bullets navigate hlinum helm-projectile helm-ag exec-path-from-shell evil-surround evil-org evil-leader evil-indent-textobject afternoon-theme))))
+    (neotree web-mode use-package rjsx-mode org-bullets navigate hlinum helm-projectile helm-ag exec-path-from-shell evil-surround evil-org evil-leader evil-indent-textobject drag-stuff afternoon-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
