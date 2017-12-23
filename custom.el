fc7233755cdba7fc376fff74219fa39d0ea68b6c
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2017-12-23 14:10:56 +0100

    ✨ Add magit and evil-magit without config

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yaml-mode web-mode use-package org-bullets neotree navigate hlinum helm-projectile helm-ag exec-path-from-shell evil-surround evil-org evil-leader evil-indent-textobject emojify drag-stuff diff-hl auto-complete afternoon-theme))))
+    (evil-magit yaml-mode web-mode use-package org-bullets neotree navigate magit hlinum helm-projectile helm-ag exec-path-from-shell evil-surround evil-org evil-leader evil-indent-textobject emojify drag-stuff diff-hl auto-complete afternoon-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
