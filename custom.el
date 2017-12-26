b7a151405ca5095c28adf2471a7915b665457c1f
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2017-12-26 22:39:01 +0100

    🔧 Use a different cursor shape for insert mode

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (evil-commentary yaml-mode web-mode use-package rust-mode org-bullets neotree navigate markdown-mode hlinum helm-projectile helm-ag exec-path-from-shell evil-surround evil-org evil-magit evil-leader evil-indent-textobject emojify drag-stuff diff-hl badger-theme auto-complete))))
+    (evil-terminal-cursor-changer yaml-mode web-mode use-package rust-mode org-bullets neotree navigate markdown-mode hlinum helm-projectile helm-ag exec-path-from-shell evil-surround evil-org evil-magit evil-leader evil-indent-textobject evil-commentary emojify drag-stuff diff-hl badger-theme auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
