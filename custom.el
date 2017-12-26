bd8d2b464e1ef16459a8d7cb7cc32db13260cfe7
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2017-12-26 17:49:07 +0100

    ✨ Add evil-commentary for evil-mode

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (rust-mode yaml-mode web-mode use-package org-bullets neotree navigate hlinum helm-projectile helm-ag exec-path-from-shell evil-surround evil-org evil-magit evil-leader evil-indent-textobject emojify drag-stuff diff-hl badger-theme auto-complete))))
+    (evil-commentary yaml-mode web-mode use-package rust-mode org-bullets neotree navigate markdown-mode hlinum helm-projectile helm-ag exec-path-from-shell evil-surround evil-org evil-magit evil-leader evil-indent-textobject emojify drag-stuff diff-hl badger-theme auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
