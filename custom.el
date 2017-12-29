2a7ba747aa40182716567119b81517b1e1e835f2
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2017-12-29 11:14:10 +0100

    🔧 Add keybindings for evil leader in org mode

--- custom.el
+++ custom.el
@@ -5,10 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-plus-contrib yaml-mode web-mode use-package rust-mode org-bullets neotree navigate markdown-mode hlinum helm-projectile helm-ag exec-path-from-shell evil-terminal-cursor-changer evil-surround evil-org evil-magit evil-leader evil-indent-textobject evil-commentary emojify drag-stuff diff-hl badger-theme auto-complete))))
+    (markdown-mode rust-mode yaml-mode web-mode diff-hl evil-magit magit emojify auto-complete neotree helm-ag helm-projectile projectile helm org-bullets org-plus-contrib navigate evil-terminal-cursor-changer evil-commentary evil-indent-textobject evil-org evil-surround evil-leader evil drag-stuff exec-path-from-shell hlinum badger-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-ellipsis ((t (:foreground nil)))))
