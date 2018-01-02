e8c631bbaff151be3c29c6155fda818445595ae8
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2018-01-02 09:38:04 +0100

    ✨ Add editorconfig plugin

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (markdown-mode rust-mode yaml-mode web-mode diff-hl evil-magit magit emojify auto-complete neotree helm-ag helm-projectile projectile helm org-bullets org-plus-contrib navigate evil-terminal-cursor-changer evil-commentary evil-indent-textobject evil-org evil-surround evil-leader evil drag-stuff exec-path-from-shell hlinum badger-theme use-package))))
+    (editorconfig markdown-mode rust-mode yaml-mode web-mode diff-hl evil-magit magit emojify auto-complete neotree helm-ag helm-projectile projectile helm org-bullets org-plus-contrib navigate evil-terminal-cursor-changer evil-commentary evil-indent-textobject evil-org evil-surround evil-leader evil drag-stuff exec-path-from-shell hlinum badger-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
