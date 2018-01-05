f186cb3c600d7bacc08a36985487421e85d0f7b2
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2018-01-05 10:43:22 +0100

    ✨ Add plugin to support GitHub pull requests on Magit

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (flycheck editorconfig markdown-mode rust-mode yaml-mode web-mode diff-hl evil-magit magit emojify auto-complete neotree helm-ag helm-projectile projectile helm org-bullets org-plus-contrib navigate evil-terminal-cursor-changer evil-commentary evil-indent-textobject evil-org evil-surround evil-leader evil drag-stuff hlinum badger-theme use-package))))
+    (magit-gh-pulls yaml-mode web-mode use-package rust-mode org-plus-contrib org-bullets neotree navigate markdown-mode hlinum helm-projectile helm-ag flycheck evil-terminal-cursor-changer evil-surround evil-org evil-magit evil-leader evil-indent-textobject evil-commentary emojify editorconfig drag-stuff diff-hl beacon badger-theme auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
