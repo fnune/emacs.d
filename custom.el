44f179b4bee33aeb242720f976a3d5e73a559c65
Fausto Núñez Alberro <fausto.nunez@mailbox.org>
2018-07-03 09:55:40 +0200

    ✨ Add GraphQL mode

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (vue-mode yaml-mode xclip web-mode use-package tide scss-mode rust-mode rainbow-delimiters prettier-js powerline org-plus-contrib org-bullets neotree navigate markdown-mode magit-gh-pulls hlinum helm-projectile helm-ag flycheck-rust evil-terminal-cursor-changer evil-surround evil-org evil-magit evil-leader evil-indent-textobject evil-commentary emojify emmet-mode editorconfig drag-stuff doom-themes diff-hl company badger-theme auto-complete))))
+    (graphql-mode yaml-mode xclip web-mode use-package tide scss-mode rust-mode rainbow-delimiters prettier-js powerline org-plus-contrib org-bullets nordless-theme nord-theme neotree navigate markdown-mode magit-gh-pulls hlinum helm-projectile helm-ag github-theme github-modern-theme flycheck-rust flatui-theme evil-terminal-cursor-changer evil-surround evil-org evil-magit evil-leader evil-indent-textobject evil-commentary emojify emmet-mode editorconfig edit-indirect drag-stuff doom-themes doom diff-hl darktooth-theme company color-theme-solarized base16-theme badger-theme auto-complete ample-theme add-node-modules-path))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
